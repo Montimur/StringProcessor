c0026759dbb33a12d59d41cc6966ebfb0d6506c5
Montimur <montimur@gmail.com>
2018-08-17 22:08:46 -0600

    Implement initial test cases.

--- src/test/java/com/montimur/AppTest.java
+++ src/test/java/com/montimur/AppTest.java
@@ -1,20 +1,41 @@
 package com.montimur;
 
-import static org.junit.Assert.assertTrue;
-
+import com.montimur.processor.StringProcessor;
 import org.junit.Test;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void testBuilder()
     {
-        assertTrue( true );
+        var builder = StringProcessor.getBuilder();
+        assertNotNull(builder);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullTest() {
+        StringProcessor.getBuilder()
+                .buildProcessor()
+                .process(null);
+    }
+
+    @Test
+    public void testTrueOne() {
+        var result = new AtomicReference<String>();
+
+        StringProcessor.getBuilder()
+                .setResultAction(result::set)
+                .buildProcessor()
+                .process(("sdf!erdcx5ksdj!kjdsf!dkjfkd!10kjkdjs!d"));
+
+        assertTrue(result.get().contains("true"));
     }
 }
